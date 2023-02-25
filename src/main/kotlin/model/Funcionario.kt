953a4882cf02c8c5458e17d5ac10de99ef7f37c3
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2023-02-24 23:08:24 -0300

    implementacao de heranca

---
+++ src/main/kotlin/model/Funcionario.kt
@@ -0,0 +1,12 @@
+package model
+
+open class Funcionario(
+    val nome: String,
+    val cpf: String,
+    val salario: Double
+) {
+
+    open fun bonificacao(): Double {
+        return salario * 0.1
+    }
+}
---
+++ src/main/kotlin/model/Gerente.kt
@@ -0,0 +1,8 @@
+package model
+class Gerente(nome: String, cpf: String, salario: Double) :
+    Funcionario(nome, cpf, salario) {
+
+    override fun bonificacao(): Double {
+        return salario * 0.2
+    }
+}
