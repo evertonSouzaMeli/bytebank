26cbdaf47830f67c98250400a453a023df0cf09b
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2023-02-25 21:41:44 -0300

    feat: add exception to project

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,3 @@
-import model.Cliente
-import model.ContaCorrente
-import model.ContaPoupanca
-import model.Gerente
-
 fun main() {
     println("Hello World")
-}
-
-fun mainConta() {
-    val contaEverton = ContaCorrente(Cliente("Everton", senha = 1234, endereco = null), numero = null)
-    val contaJoao = ContaPoupanca(Cliente("João", senha = 4567, endereco = null), numero = null)
-
-
-    contaJoao.depositar(10.0)
-    contaJoao.transferir(6.0, contaEverton)
-
-    contaEverton.depositar(10.0)
-    contaEverton.sacar(5.0)
-
-
-    println("Nome: ${contaEverton.titular}, \nNumero: ${contaEverton.numero} \nSaldo: ${contaEverton.saldo}")
-    println("Nome: ${contaJoao.titular}, \nNumero: ${contaJoao.numero} \nSaldo: ${contaJoao.saldo}")
 }
--- src/main/kotlin/model/Endereco.kt
+++ src/main/kotlin/model/Endereco.kt
@@ -1,12 +1,14 @@
 package model
 
 class Endereco(
-    val logradouro: String,
-    val numero: Int,
-    val bairro: String,
-    val cidade: String,
-    val estado: String,
-    val cep: String
+    private val logradouro: String,
+    private val numero: Int,
+    private val bairro: String,
+    private val cidade: String,
+    private val estado: String,
+    private val cep: String
 ) {
-
+    override fun toString(): String {
+        return "Endereco(logradouro='$logradouro', numero=$numero, bairro='$bairro', cidade='$cidade', estado='$estado', cep='$cep')"
+    }
 }
