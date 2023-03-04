c6b9e585a266b0e9ac9f9b9fcc32de06fed8e112
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2023-03-03 22:45:40 -0300

    feat: working with multiple returns in lambda and anonymous function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,60 @@
 fun main() {
     println("Hello World")
 
-    val minhaFuncaoLambda: () -> Unit =  {
-        println("Executa como lambda")
+    val minhaFuncaoLambda: (Int, Int) -> Int = { _, b: Int ->
+        b + 10
     }
 
-    println(minhaFuncaoLambda())
+    val minhaFuncaoLambda2 = { a: Int, b: Int ->
+        a + b
+    }
 
-    val minhaFuncaoAnonima: () -> Unit = fun () {
+    println(minhaFuncaoLambda(15, 10))
+
+    val minhaFuncaoAnonima: (Int, Int) -> Int = fun(_, b: Int): Int {
         println("Executa como anonima")
+        return b + 2
     }
 
     val safeDivide = { numerator: Int, denominator: Int ->
-        if(denominator == 0 ) 0.0 else numerator.toDouble() / denominator
+        if (denominator == 0) 0.0 else numerator.toDouble() / denominator
     }
+
+    val calculaBonificacao: (salario: Double) -> Double = lambda@ {salario ->
+        if( salario > 1000.0)
+            return@lambda salario + 50
+
+        salario + 100.0
+    }
+
+    println(calculaBonificacao(1100.0))
+
+    val calculadoraBonificacaoFuncaoAnonima: (salario: Double) -> Double = fun(salario): Double {
+        if(salario >  1000.0)
+            return salario + 50
+
+        return salario + 100.0
+    }
+
+    println(calculadoraBonificacaoFuncaoAnonima(1100.0))
 }
 
 private fun testaFuncaoClasse() {
-    val minhaFuncaoClasses: () -> Unit = Teste()
+    val minhaFuncaoClasses: (Int, Int) -> Int = Soma()
 
-    println(minhaFuncaoClasses())
+    println(minhaFuncaoClasses(1, 2))
 }
 
 private fun testaFuncaoReferencia() {
-    val minhaFuncao: () -> Unit = ::teste
+    val minhaFuncao: (Int, Int) -> Int = ::soma
 
-    println(minhaFuncao())
+    println(minhaFuncao(5, 10))
 }
 
-fun teste() {
-    println("executa teste")
+fun soma(a: Int, b: Int): Int {
+    return a + b
 }
 
-class Teste: () -> Unit {
-    override fun invoke() {
-        println("executa invoke do Teste")
-    }
+class Soma : (Int, Int) -> Int {
+    override fun invoke(a: Int, b: Int): Int = a + b
 }
