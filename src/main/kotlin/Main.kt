9bd3afdf4e479764fff5489745c18cd7fc977c4e
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2023-03-03 21:42:04 -0300

    feat: start to use func type

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,39 @@
 fun main() {
     println("Hello World")
+
+    val minhaFuncaoLambda: () -> Unit =  {
+        println("Executa como lambda")
+    }
+
+    println(minhaFuncaoLambda())
+
+    val minhaFuncaoAnonima: () -> Unit = fun () {
+        println("Executa como anonima")
+    }
+
+    val safeDivide = { numerator: Int, denominator: Int ->
+        if(denominator == 0 ) 0.0 else numerator.toDouble() / denominator
+    }
+}
+
+private fun testaFuncaoClasse() {
+    val minhaFuncaoClasses: () -> Unit = Teste()
+
+    println(minhaFuncaoClasses())
+}
+
+private fun testaFuncaoReferencia() {
+    val minhaFuncao: () -> Unit = ::teste
+
+    println(minhaFuncao())
+}
+
+fun teste() {
+    println("executa teste")
+}
+
+class Teste: () -> Unit {
+    override fun invoke() {
+        println("executa invoke do Teste")
+    }
 }
